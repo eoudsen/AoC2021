d3c664c4d3e22a7ee026e80cf5daabb0c4e1b84f
Erik Oudsen <e.oudsen@gmail.com>
2021-12-02 08:17:09 +0100

    Solution day 2 and preparation day 3

--- day2/day2_2.py
+++ day2/day2_2.py
@@ -3,19 +3,21 @@ def solve():
     lines = open('input.txt').readlines()
     horizontal = 0
     depth = 0
+    aim = 0
     for line in lines:
-        horizontal, depth = accept_command(line, horizontal, depth)
+        horizontal, depth, aim = accept_command(line, horizontal, depth, aim)
     return horizontal, depth
 
 
-def accept_command(command, horizontal, depth):
+def accept_command(command, horizontal, depth, aim):
     if command.startswith("forward"):
         horizontal += int(command.split(" ")[1])
+        depth += aim * int(command.split(" ")[1])
     elif command.startswith("down"):
-        depth += int(command.split(" ")[1])
+        aim += int(command.split(" ")[1])
     elif command.startswith("up"):
-        depth -= int(command.split(" ")[1])
-    return horizontal, depth
+        aim -= int(command.split(" ")[1])
+    return horizontal, depth, aim
 
 
 if __name__ == "__main__":
