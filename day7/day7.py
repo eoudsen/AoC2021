0430b7adce3a64635cc2ae7bc957dab2efd1eecb
Erik Oudsen <e.oudsen@gmail.com>
2021-12-07 08:08:59 +0100

    Solution day 7, prepare day 8

--- day7/day7.py
+++ day7/day7.py
@@ -1,9 +1,22 @@
-
+import math
+from statistics import median
+from statistics import mean
 
 def solve():
-    lines = open('input.txt').readlines()
+    numbers = [int(x) for x in open('input.txt').readline().split(",")]
+    middle = median(numbers)
+    distances = [abs(x - middle) for x in numbers]
+    return sum(distances)
+
+
+def solve2():
+    numbers = [int(x) for x in open('input.txt').readline().split(",")]
+    rounded_mean = math.floor(mean(numbers)) if mean(numbers) % 1 < .75 else round(mean(numbers))
+    distances = [sum(range(abs(x - rounded_mean) + 1)) for x in numbers]
+    return sum(distances)
 
 
 if __name__ == "__main__":
     print(solve())
+    print(solve2())
     
